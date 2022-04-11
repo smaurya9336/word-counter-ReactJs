5edab8dc9eba0062441afe0bf5b8ec8f29486246
Sarita Maurya <77966720+smaurya9336@users.noreply.github.com>
2022-04-11 21:05:28 +0530

    javascript

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+  <>
+{/* <Navbar title="TextUtils" AboutText="About TextUtils"/> */}
+{/*<Navbar/>*/}
+<Navbar title="TextUtils" />
+<div className="container my-3">
+<TextForm heading="Enter the text to analyze below"/>
+</div>
+  </>
+  );
+}
+
+export default App;
